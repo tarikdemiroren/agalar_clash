08612c3c2aa0f4963a2251c8d4970a8d5cabde6d
tarikdemiroren <tarekarez5652@gmail.com>
2023-01-27 17:08:42 +0300

    movement of player improved

--- agalar_clash_code/src/Actors/Player/Actors.gd
+++ agalar_clash_code/src/Actors/Player/Actors.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 class_name Actors
 
 var gravity = 1500
-var speed = 500
+var jump_force = 600
+var speed = 50
 var max_falling_speed = 1500
 
 # Called when the node enters the scene tree for the first time.
--- agalar_clash_code/src/Actors/Player/Player.gd
+++ agalar_clash_code/src/Actors/Player/Player.gd
@@ -1,44 +1,49 @@
 extends Actors
 # Called when the node enters the scene tree for the first time.
-var max_speed = 1000
+var max_speed = 800
 var velocity: Vector2 = Vector2.ZERO
-var is_just_on_wall = false
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+
+	calcwelocity()
 	
-	if is_on_wall() or is_on_floor() or is_on_ceiling():
-		velocity = speed * calculateInput()
-		
-	if velocity.y <= max_falling_speed:
+	if not velocity.y > max_falling_speed and not is_on_wall():
 		velocity.y += gravity * delta
 	
-	set_just_on_wall()
-	if is_just_on_wall:
-		velocity.y = 500
+	if velocity.x >= 0:
+		velocity.x = min(velocity.x, max_speed)
+	else:
+		velocity.x = max(velocity.x, -max_speed)
 	
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity = dissipateVelocity(velocity, delta)
 
 func calculateInput() -> Vector2:
 	var vector: Vector2 = Vector2(0,0)
 	vector.x= Input.get_action_strength("go_right") - Input.get_action_strength("go_left")
 	
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_pressed("jump") and is_on_floor():
 		vector.y = -1
+
 	return vector
 
-func set_just_on_wall() -> void:
-	if is_on_wall() and not is_just_on_wall:
-		is_just_on_wall = true
-	elif is_on_wall() and is_just_on_wall:
-		is_just_on_wall = false
+func calcwelocity() -> void:
+	if (is_on_floor()):
+		velocity.x += speed * calculateInput().x
+		velocity.y += jump_force * calculateInput().y
+	elif (not is_on_wall() or not is_on_ceiling()):
+		velocity.x += speed * calculateInput().x * 0.7
 	
 func dissipateVelocity(vector: Vector2, delta: float) -> Vector2:
-	if is_on_wall() and vector.y >= 0:
-		vector.y -= 100*delta
-	vector.x -= 20*delta 
+	if is_on_wall() and vector.y <= 0:
+		vector.y += 10*delta
+	if is_on_floor():
+		if vector.x > 0:
+			vector.x -= 20
+		elif vector.x < 0:
+			vector.x += 20
 	return vector
